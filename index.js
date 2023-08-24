343b2cca767039f9c63f666b7802a2fc0fa791b7
Martin Viera <vieramaster@hotmail.com>
2023-08-23 21:05:52 -0300

    agregado de noticias, banners por actualizacion

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
-
-const modal = document.querySelector("#modal")
-
-const btnOpenModal = document.querySelector("#btn-open-modal");
-
-const btnCloseModal = document.querySelector("#btn-close-modal");
-document.querySelector("#modal");
+const responsiveOpen = document.querySelector("#responsiveOpen")
+const responsiveClose = document.querySelector("#responsiveClose")
+const element = document.querySelector('.header__box');
+const shadow = document.querySelector("header")
 
 
-btnOpenModal.addEventListener("click", ()=> {
-modal.showModal();
-})
+responsiveOpen.addEventListener('click', () => {
+    shadow.classList.add("shadow")
+    element.classList.add('header__box-open');
+    document.body.style.overflow = 'hidden';
+  });
 
-btnCloseModal.addEventListener("click", () => {
-modal.close();
+  function closeResponsive(){
+    shadow.classList.remove("shadow")
+    element.classList.remove('header__box-open');
+    document.body.style.overflow = 'scroll';
+  }
+
+responsiveClose.addEventListener("click", closeResponsive)
+
+window.addEventListener('resize', function() {
+  let width = window.innerWidth;
+  if (width < 1050) {
+      closeResponsive();
+  }
 });
 
 
-document.getElementById("btn-open-modal").addEventListener("click", function () {
-    document.body.classList.add("open-modal");
-});
-document.getElementById("btn-close-modal").addEventListener("click", function () {
-    document.body.classList.remove("open-modal");
-});
 
+/*----------------------------------MODAL-------------------------------------------*/
+
+  const modal = document.querySelector("#modal")
+
+  const btnOpenModal = document.querySelector("#btn-open-modal");
+  
+  const btnCloseModal = document.querySelector("#btn-close-modal");
+  document.querySelector("#modal");
+  
+  
+  btnOpenModal.addEventListener("click", ()=> {
+  modal.showModal();  modal.classList.add('active');
+  })
+  
+  btnCloseModal.addEventListener("click", () => {
+  modal.close();  modal.classList.remove('active');
+  });
+  
+  
+/*-------------------------------ul--------------------------------------------------*/
+
+const openList = document.querySelector(".header__nav--recursos")
+const list = document.querySelector (".subMenu")
+
+openList.addEventListener("click", () =>{
+  
+  if(list.style.display === "none"){
+    list.style.display = "block"
+  }
+  else{
+    list.style.display = "none";
+  }
+});
 
